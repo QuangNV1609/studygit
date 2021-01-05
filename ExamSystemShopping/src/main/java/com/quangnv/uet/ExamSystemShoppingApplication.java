cb6265e60a92123189c94dd48c6ecae43bc9c4af
QuangNVUET1609 <nguyenvanquang16092001@gmail.com>
2021-01-05 19:42:51 +0700

    create project

---
+++ ExamSystemShopping/src/main/java/com/quangnv/uet/ExamSystemShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.quangnv.uet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSystemShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSystemShoppingApplication.class, args);
+	}
+
+}
---
+++ ExamSystemShopping/src/test/java/com/quangnv/uet/ExamSystemShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangnv.uet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamSystemShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
